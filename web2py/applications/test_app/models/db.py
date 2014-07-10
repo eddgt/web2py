f6c569983ee4499c3ed5b75975dee7dd0c6d2e61
eddgt <edmundo_ulloa@hotmail.com>
2014-07-10 12:25:08 -0600

    web2py final commit

---
+++ web2py/applications/test_app/models/db.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+
+#########################################################################
+## This scaffolding model makes your app work on Google App Engine too
+## File is released under public domain and you can use without limitations
+#########################################################################
+
+## if SSL/HTTPS is properly configured and you want all HTTP requests to
+## be redirected to HTTPS, uncomment the line below:
+# request.requires_https()
+
+if not request.env.web2py_runtime_gae:
+    ## if NOT running on Google App Engine use SQLite or other DB
+    #db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
+    db=DAL('mysql://root:@localhost/test')
+else:
+    ## connect to Google BigTable (optional 'google:datastore://namespace')
+    db = DAL('google:datastore')
+    ## store sessions and tickets there
+    session.connect(request, response, db=db)
+    ## or store session in Memcache, Redis, etc.
+    ## from gluon.contrib.memdb import MEMDB
+    ## from google.appengine.api.memcache import Client
+    ## session.connect(request, response, db = MEMDB(Client()))
+
+## by default give a view/generic.extension to all actions from localhost
+## none otherwise. a pattern can be 'controller/function.extension'
+response.generic_patterns = ['*'] if request.is_local else []
+## (optional) optimize handling of static files
+# response.optimize_css = 'concat,minify,inline'
+# response.optimize_js = 'concat,minify,inline'
+## (optional) static assets folder versioning
+# response.static_version = '0.0.0'
+#########################################################################
+## Here is sample code if you need for
+## - email capabilities
+## - authentication (registration, login, logout, ... )
+## - authorization (role based authorization)
+## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
+## - old style crud actions
+## (more options discussed in gluon/tools.py)
+#########################################################################
+
+from gluon.tools import Auth, Crud, Service, PluginManager, prettydate
+auth = Auth(db)
+crud, service, plugins = Crud(db), Service(), PluginManager()
+
+## create all tables needed by auth if not custom tables
+auth.define_tables(username=False, signature=False)
+
+## configure email
+mail = auth.settings.mailer
+mail.settings.server = 'logging' or 'smtp.gmail.com:587'
+mail.settings.sender = 'you@gmail.com'
+mail.settings.login = 'username:password'
+
+## configure auth policy
+auth.settings.registration_requires_verification = False
+auth.settings.registration_requires_approval = False
+auth.settings.reset_password_requires_verification = True
+
+## if you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
+## register with janrain.com, write your domain:api_key in private/janrain.key
+from gluon.contrib.login_methods.rpx_account import use_janrain
+use_janrain(auth, filename='private/janrain.key')
+
+#########################################################################
+## Define your tables below (or better in another model file) for example
+##
+## >>> db.define_table('mytable',Field('myfield','string'))
+##
+## Fields can be 'string','text','password','integer','double','boolean'
+##       'date','time','datetime','blob','upload', 'reference TABLENAME'
+## There is an implicit 'id integer autoincrement' field
+## Consult manual for more options, validators, etc.
+##
+## More API examples for controllers:
+##
+## >>> db.mytable.insert(myfield='value')
+## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
+## >>> for row in rows: print row.id, row.myfield
+#########################################################################
+
+
+# PRODUCTS and CUSTOMERS will be added to the database automatically
+# as dummy data
+
+PRODUCTS = [("spamelon", 10.34, "Kg"), ("banana", 32.32, "Kg"),
+            ("waterspamelon", 2.32, "Kg"), ("cherry", 32.33, "Kg"),
+            ("raspberry", 19.34, "Kg"), ("pear", 23.21, "Kg"),
+            ("pineapple", 24.12, "Kg"), ("orange", 12.34, "Kg"),
+            ("grapefruit", 10.03, "Kg"), ("tangerine", 20.30, "Kg"),
+            ("apple", 02.23, "Kg"), ("fig", 20.24, "Kg"),
+            ("kiwi", 10.02, "Kg"), ("strawberry", 20.21, "Kg"),
+            ("discount", -1.00, "units"), ("charge", 1.00, "units" )]
+
+CUSTOMERS = [("Michael", "23423084230"), ("John", "320984230984"),
+             ("Graham", "234092349"), ("Terry", "23094230"),
+             ("Eric", "0923840293")]
+
+# STATUSES indicates wether an invoice can be edited (is open) or
+# it is closed. Closed operations can be locked for archive purposes
+# or cancelled
+
+PRODUCTOS = [("spamelon", 10.34, "Kg"), ("banana", 32.32, "Kg"),
+            ("waterspamelon", 2.32, "Kg"), ("cherry", 32.33, "Kg"),
+            ("raspberry", 19.34, "Kg"), ("pear", 23.21, "Kg"),
+            ("pineapple", 24.12, "Kg"), ("orange", 12.34, "Kg"),
+            ("grapefruit", 10.03, "Kg"), ("tangerine", 20.30, "Kg"),
+            ("apple", 02.23, "Kg"), ("fig", 20.24, "Kg"),
+            ("kiwi", 10.02, "Kg"), ("strawberry", 20.21, "Kg"),
+            ("discount", -1.00, "units"), ("charge", 1.00, "units" )]
+
+CLIENTES = [("Michael", "23423084230"), ("John", "320984230984"),
+             ("Graham", "234092349"), ("Terry", "23094230"),
+             ("Eric", "0923840293")]
+
+
+
+STATUSES = {"open": T("Open"), "locked": T("Locked"),
+            "cancelled": T("Cancelled")}
+
+ESTADOS = {"abierta": T("Abierta"), "bloqueada": T("Bloqueada"),
+            "cancelada": T("Cancelada")}
+
+PAYMENTS = ["cash", "credit card", "other"]
+
+CONTRIBUYENTES = ["efectivo", "tarjeta", "otro"]
+
+UNITS = ["units", "Kg", "g", "l"]
+
+UNIDADES = ["unds", "Kg", "g", "l"]
+
+
+## after defining tables, uncomment below to enable auditing
+# auth.enable_record_versioning(db)
+
+# The bill's recipient
+db.define_table("customer",
+                Field("name"),
+                Field("taxpayer"),
+                format="%(name)s (%(taxpayer)s)")
+
+
+db.define_table("invoice",
+                Field("created_on", "datetime", default=request.now),
+                Field("created_by", "reference auth_user",
+                      default=auth.user_id, writable=False),
+                Field("customer_id", "reference customer"),
+                Field("description"),
+                Field("total", "double",
+                      writable=False, default=0.00),
+                Field("payment", requires=IS_IN_SET(PAYMENTS)),
+                Field("status", requires=IS_IN_SET(STATUSES),
+                      default="open", writable=False),
+                format="%(id)s")
+
+db.define_table("concepto",
+                Field("nombre"),
+                Field("descripcion"),
+                Field("monto", "double", default=0.00),
+                Field("medida", requires=IS_IN_SET(UNIDADES),
+                      default="unds"),
+                format="%(nombre)s: $%(monto)s (%(medida)s)")
+
+
+db.define_table("cliente",
+                Field("nombre"),
+                Field("contribuyente"),
+                format="%(nombre)s (%(contribuyente)s)")
+
+db.define_table("factura",
+                Field("creada_en", "datetime", default=request.now),
+                Field("creada_por", "reference auth_user",
+                      default=auth.user_id, writable=False),
+                Field("cliente_id", "reference cliente"),
+                Field("descripcion"),
+                Field("total", "double",
+                      writable=False, default=0.00),
+                Field("tipopago", requires=IS_IN_SET(CONTRIBUYENTES)),
+                Field("status", requires=IS_IN_SET(ESTADOS),
+                      default="abierto", writable=False),
+                format="%(id)s")
+
+db.define_table("item",
+                Field("factura_id", "reference factura",
+                      default=session.factura_id,
+                      writable=False),
+                Field("concepto_id", "reference concepto"),
+                Field("monto", "double", default=0.00),
+                Field("total", "double", default=0.00,
+                      writable=False),
+                format=lambda record: record.concepto_id.nombre)
+
+
+# Here we link invoice ids to the invoice details page.
+db.factura.id.represent = lambda id, field: \
+    A(id, _href=URL(c="default", f="factura", args=["factura", id]))
+
+# Automatically add dummy data (not recommended in production)
+if db(db.cliente).count() <= 0:
+    for cliente in CLIENTES:
+        db.cliente.insert(nombre=cliente[0],
+                           contribuyente=cliente[1])
+    for producto in PRODUCTOS:
+       db.concepto.insert(nombre=producto[0], monto=producto[1],
+                         medida=producto[2])
+
+# This will be called by the business logic to update open invoices.
+# It computes the invoice items and updates the total amounts
+def factura_total(factura):
+    if factura.status in ("bloqueada", "cancelada"): return
+    items = db(db.item.factura_id==factura.id).select()
+    total = 0.00
+    for item in items:
+        item_total = item.monto * item.concepto_id.monto
+        item.update_record(total=item_total)
+        total += item_total
+    factura.update_record(total=total)
